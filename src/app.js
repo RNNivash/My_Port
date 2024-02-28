ac5b3a8ae3850342e681bb5c4271ed8f9887fd46
RNNivash <rajnivash007@gmail.com>
2024-02-28 21:18:00 +0530

    Initial  commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+const blog = document.getElementById('blog')
+const online = document.getElementById('online')
+const attendance = document.getElementById('attendance')
+const investment = document.getElementById('investment')
+const weather = document.getElementById('weather')
+
+
+
+online.addEventListener('click', () => {
+    window.open('https://github.com/RNNivash/Eatry.git', '_blank')
+})
+
+attendance.addEventListener('click', () => {
+    window.open('https://github.com/RNNivash/Smart_attendance_system', '_blank')
+})
+
+weather.addEventListener('click', () => {
+    window.open('https://github.com/RNNivash/Sky_scope', '_blank')
+})
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click',toggleButton)
+
